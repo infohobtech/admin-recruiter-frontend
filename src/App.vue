1153031270e2eb826d9d88d68d99bddc6b9c52e8
Md. Mamunur Rashid Rimon <rimonmath@gmail.com>
2022-09-30 12:29:52 +0600

    refreshToken feature added

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,37 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 // import HelloWorld from './components/HelloWorld.vue'
+import { onMounted } from "vue";
 import Toasts from "./components/overlay/Toasts.vue";
+
+import authService from "./services/authService";
+
+import { setAdminHeaders } from "./services";
+
+onMounted(() => {
+  refreshToken();
+});
+
+let timer = 0;
+
+function refreshToken() {
+  timer = setTimeout(refreshToken, 4 * 60 * 1000);
+
+  const token = localStorage.getItem("token");
+  const rt = localStorage.getItem("refreshToken");
+
+  if (!token || !rt) {
+    return;
+  }
+
+  authService
+    .refreshToken(rt)
+    .then((res) => {
+      setAdminHeaders(res.data.token);
+    })
+    .catch()
+    .finally(() => {});
+}
 </script>
 
 <template>
